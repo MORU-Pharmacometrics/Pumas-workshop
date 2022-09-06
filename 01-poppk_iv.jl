6231b5e12fa912e7a25f4737195a1e1d937746ab
Jose Storopoli <jstoropoli@protonmail.com>
2022-09-06 09:03:25 +0000

    some changes to 01-poppk_iv.jl

--- 01-poppk_iv.jl
+++ 01-poppk_iv.jl
@@ -3,7 +3,6 @@
 # Load the necessary Libraries
 using Pumas
 using PumasUtilities
-using Pumas.Latexify
 using PharmaDatasets
 
 # Read data
@@ -32,7 +31,6 @@ model = @model begin
         # pre computations and other statistical transformations
         CL = tvcl * exp(η[1])
         Vc = tvvc * exp(η[2])
-        # myvar = if t == 5 0 else 1 end
     end
 
     # here we define compartments and dynamics
@@ -43,26 +41,25 @@ model = @model begin
         # tilde (~) means "distributed as"
         cp = @. 1000 * Central / Vc # ipred = A1/V
         dv ~ @. Normal(cp, σ)
-        # CONC ~ @. Normal(cp, sqrt(cp^2 * σ_prop^2 + σ_add^2))
+        # dv ~ @. Normal(cp, sqrt(cp^2 * σ_prop^2 + σ_add^2))
     end
 end
-
-# render
-render(latexify(model, :param))
 
 # Parameter values
 params = (tvcl = 1.0, tvvc = 10.0, Ω = Diagonal([0.09, 0.09]), σ = 3.16)
 params2 = (tvcl = 1.0, tvvc = 8.0, Ω = Diagonal([0.5, 0.5]), σ = 4.16)
 
 # Fit a base model
-fit_results = fit(model, population, params, Pumas.FOCE())             # using custom initial parameter values
-fit_results2 = fit(model, population, params, Pumas.NaivePooled(); omegas = (:Ω,))             # using custom initial parameter values
-fit_results3 = fit(model, population, init_params(model), Pumas.FOCE()) # using the model's initial parameter values
+fit_results = fit(model, population, params, Pumas.FOCE())
+fit_results2 = fit(model, population, params, Pumas.NaivePooled(); omegas = (:Ω,))
+fit_results3 = fit(model, population, params2, Pumas.LaplaceI())
+fit_results4 = fit(model, population, params2, Pumas.FOCE(); constantcoef=(tvcl = 1.0,))
 
 fit_compare = compare_estimates(;
-    custom_params = fit_results,
-    init_params = fit_results3,
+    FOCE = fit_results,
+    LaplaceI = fit_results3,
     NaivePooled = fit_results2,
+    FOCE_constantcoef = fit_results4
 )
 
 # Confidence Intervals
@@ -71,17 +68,23 @@ coeftable(fit_infer)  # DataFrame
 
 # Confidence Intervals using bootstrap
 fit_infer_bs = infer(fit_results, Pumas.Bootstrap(samples = 100))
-coeftable(fit_infer)
+coeftable(fit_infer_bs)
 
+#SIR
+fit_infer_sir = infer(fit_results, Pumas.SIR(samples=10, resamples=10))
+coeftable(fit_infer_sir)
+
+# Inspect the models
 fit_inspect = inspect(fit_results)
 fit_inspect2 = inspect(fit_results2)
 fit_inspect3 = inspect(fit_results3)
 fit_diagnostics = evaluate_diagnostics((;
-    custom_params = fit_inspect,
+    FOCE = fit_inspect,
     NaivePooled = fit_inspect2,
-    init_params = fit_inspect3,
+    LaplaceI = fit_inspect3,
 ))
 
+# VPCs
 fit_vpc = vpc(fit_results) # Single-Threaded
 fit_vpc = vpc(
     fit_results; # Multi-Threaded
@@ -90,6 +93,7 @@ fit_vpc = vpc(
 
 vpc_plot(fit_vpc)
 
+# Pumas Apps
 fit_diagnostics = evaluate_diagnostics((fit_inspect, fit_vpc),)
 
 model_explore = explore_estimates(model, population, params)
