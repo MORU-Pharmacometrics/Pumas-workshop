5e5734ba274358c246eb3155a0ddddbd45eb7919
Jose Storopoli <jstoropoli@protonmail.com>
2022-09-20 09:20:42 +0000

    created 00-interactive_apps.jl

---
+++ 00-interactive_apps.jl
@@ -0,0 +1,14 @@
+# Pumas users can use Interactive Web Applications such as:
+# - Data Loader
+# - Model Builder
+# - Population Builder
+# - Estimates
+# - Diagnostics
+
+using PumasApps
+
+# create a workspace
+ws1 = PumasApps.Workspace.new()
+
+# close a workspace
+close(ws1)
